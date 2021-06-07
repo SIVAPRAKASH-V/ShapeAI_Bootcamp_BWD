a79721144b5b3bd08dfaf418639950aaf73a8fbf
SIVAPRAKASH-V <85498944+SIVAPRAKASH-V@users.noreply.github.com>
2021-06-07 18:59:02 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> AMAZING BOOTCAMPS</h1>
+      <p>
+      Join a network of hundreds of students from across the country to support our motive of Learn, Connect, and Grow!
+      </p>
+    </div>
+  );
+}
+
+export default Note;
+  
